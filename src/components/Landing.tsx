350df5e696a34a0f95a84bcc165ddef5fe41a231
Sherif Mohammed El Sayed <SHERIFMOHAMMEDELSAYED6@gmail.com>
2026-08-18 10:10:42 +0300

    All in

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,7 +9,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              SHERIF
+              Sherif
               <br />
               <span>MOHAMMED</span>
             </h1>
